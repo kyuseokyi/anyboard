77bc9e03e564c4f578e944a23cf94d74f9a3d1ff
kyuseokyi <kyuseok.y@gmail.com>
2021-11-17 01:24:52 +0900

    add webapck

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    // console.log(config)
+    return config
+  },
 }
